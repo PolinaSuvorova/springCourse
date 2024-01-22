f1792cc7b563fa309fbdaa322837c168a6b3e7c0
PolinaSuvorova <polina.tatara@gmail.com>
2024-01-22 12:05:25 +0300

    dependency injection by setter set property value

--- src/main/java/com/polinasuvorova/spring_course/rootclass/Person.java
+++ src/main/java/com/polinasuvorova/spring_course/rootclass/Person.java
@@ -2,13 +2,24 @@ package com.polinasuvorova.spring_course.rootclass;
 
 public class Person {
     private Pet pet;
-
+    private String surname;
+    private Integer age;
     public Person() {
         System.out.println("Create person");
         //this.pet = pet;
     }
 
-    public void setPet( Pet pet ) {
+    public void setSurname(String surname) {
+        System.out.println("Set surname");
+        this.surname = surname;
+    }
+
+    public void setAge(Integer age) {
+        System.out.println("Set age");
+        this.age = age;
+    }
+
+    public void setPet(Pet pet ) {
         System.out.println("Set class pet");
      //   this.pet = pet;
     }
