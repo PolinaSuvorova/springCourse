8d0190946cfbcdaef264601852ff3d93fe98eb56
PolinaSuvorova <polina.tatara@gmail.com>
2024-02-16 11:40:38 +0300

    Work with relation one-to-one in objects. Launch Crud Operation

---
+++ src/main/java/com/polinasuvorova/hibernatelesson/entity/EmployeeTestOneToOne.java
@@ -0,0 +1,41 @@
+package com.polinasuvorova.hibernatelesson.entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class EmployeeTestOneToOne {
+    public static void main(String[] args) {
+        //Создаём сессии
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+        try {
+
+            Employee employee = new Employee("Polina", "TTT", "HR", 300000);
+            Detail empDetail = new Detail("Moscow", "8888888", "polina@gmail.com");
+            employee.setEmpDetail(empDetail);
+            // открытие транзакции и изменение данных
+            session.beginTransaction();
+            session.persist(employee);
+            session.getTransaction().commit();
+            int myId = employee.getId();
+
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            Employee empGet = session.get(Employee.class, myId);
+            System.out.println(empGet.getEmpDetail());
+            session.remove(empGet);
+            session.getTransaction().commit();
+
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+
+    }
+}
